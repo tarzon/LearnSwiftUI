bde963e376c631bcccb49f7e20f6aa9887db976e
Nikhil <nikhil0803@gmail.com>
2021-05-29 22:18:26 +0530

    Added Custom View Containers, just like creating a custom View that could accept any kind of View and display it.

--- Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -49,18 +49,27 @@ extension View {
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 30) {
-            Text("Hello, world!")
-                .modifier(Title())
+        VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 30) {
+                Text("Hello, world!")
+                    .modifier(Title())
+                
+                Text("Extension of a View Modifier")
+                    .titleStyle()
+                
+                Text("Custom View Modfier")
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .titleStyle()
+                    .watermark(text: "Nikhil")
+                Spacer()
+            }
             
-            Text("Extension of a View Modifier")
-                .titleStyle()
-            
-            Text("Custom View Modfier")
-                .frame(width: 300, height: 300, alignment: .center)
-                .titleStyle()
-                .watermark(text: "Nikhil")
-            Spacer()
+            GridStack(rows: 4, columns: 4) { (rows, cols) in
+                HStack {
+                    Image(systemName: "\(rows * 4 + cols).circle").renderingMode(.original)
+                    Text("R\(rows), C\(cols)")
+                }
+            }.background(Color.pink)
         }
     }
 }
@@ -68,5 +77,26 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// Custom View Container
+
+// Uses Genrics
+struct GridStack<Content:View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) {rows in
+                HStack {
+                    ForEach(0 ..< columns) { cols in
+                        self.content(rows, cols)
+                    }
+                }
+            }
+        }
     }
 }
